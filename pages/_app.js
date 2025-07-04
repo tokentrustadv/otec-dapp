0c662f9f897857b1a83567de4aafe3f3c1fc5b5c
Charles <117126108+tokentrustadv@users.noreply.github.com>
2025-07-04 15:53:09 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,42 @@
 import '../styles/globals.css';
-import { WagmiConfig, createClient, configureChains, mainnet } from 'wagmi';
-import { publicProvider } from 'wagmi/providers/public';
+import '@rainbow-me/rainbowkit/styles.css';
 import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
+import { publicProvider } from 'wagmi/providers/public';
 import { base } from 'wagmi/chains';
+import { Toaster } from 'react-hot-toast';
 
-const { chains, provider } = configureChains(
+const { chains, publicClient } = configureChains(
   [base],
-  [publicProvider()]
+  [
+    jsonRpcProvider({
+      rpc: () => ({ http: 'https://mainnet.base.org' }),
+    }),
+    publicProvider(),
+  ]
 );
 
 const { connectors } = getDefaultWallets({
   appName: 'OTEC Licensing Dapp',
-  chains
+  projectId: 'ced749b38222900677e11e8d3b875b2e', // reuse or update project ID
+  chains,
 });
 
-const wagmiClient = createClient({
+const wagmiConfig = createConfig({
   autoConnect: true,
   connectors,
-  provider
+  publicClient,
 });
 
 export default function App({ Component, pageProps }) {
   return (
-    <WagmiConfig client={wagmiClient}>
+    <WagmiConfig config={wagmiConfig}>
       <RainbowKitProvider chains={chains}>
-        <Component {...pageProps} />
+        <main className="font-sans">
+          <Component {...pageProps} />
+        </main>
+        <Toaster position="top-right" />
       </RainbowKitProvider>
     </WagmiConfig>
   );
